17ed874e7203cd6fddac6d5973801258271fe5a0
julchristley <156378804+julchristley@users.noreply.github.com>
2024-01-20 20:22:56 -0700

    Create practice1.cs

---
+++ practicefirstmodule.cs
@@ -0,0 +1,57 @@
+using System;
+
+namespace figuringStuffOut
+{
+class Program
+{
+static void Main(string[] args)
+{
+Console.WriteLine("Hello World!"); // I am commenting
+
+Console.WriteLine("figuring it out"); 
+
+/* i am learning. commenting commenting to get to two lines. typing typing typing typing finally ok */
+
+Console.WriteLine("i'm still not too sure how to use git");
+
+string name = "pepper";
+Console.WriteLine(name);
+
+Console.WriteLine("   ");
+
+int myNumOne = 13;
+myNumOne = 27;
+Console.WriteLine(myNumOne);
+
+Console.WriteLine(" ");
+Console.WriteLine("howdy " + name + "!");
+
+Console.WriteLine("   ");
+
+string firstName = "john";
+string lastName = " doe";
+string fullName = firstName +lastName;
+Console.WriteLine(fullName);
+
+int x = 9;
+int y = 4, z = -10;
+Console.WriteLine(x+y+z);
+
+Console.WriteLine("   ");
+
+float myNumTwo = 5.75F;
+Console.WriteLine(myNumTwo);
+
+double myNumThree = 19.99D;
+Console.WriteLine(myNumThree);
+
+float f1 = 35e3f;
+double d1 = 12E4d;
+Console.WriteLine(f1);
+Console.WriteLine(d1);
+
+bool isOrangeTheBestColor = true;
+Console.WriteLine(isOrangeTheBestColor);
+}
+}
+}
