e4e3beb1fa6cf7710bc7390a358417ed057a9f1a
julchristley <156378804+julchristley@users.noreply.github.com>
2024-01-30 20:05:14 -0700

    Update lab1b.cs

--- lab1b.cs
+++ lab1b.cs
@@ -1,40 +1,19 @@
 using System;
 public class Program
 {
-public void Main ()
-{
-Console.WriteLine("Is the current temperature over 30 degrees celsius? Yes or No?");
-string temperature =  Console.ReadLine( );
-	CheckTemperature(temperature);
-	
-	}
-public void CheckTemperature (string temperature) {
-	if(temperature == "yes") {
-		Console.WriteLine("Heat stroke is serious. Make sure to drink water and stay inside.");
-	} else {
-		Console.WriteLine("The weather must be nice! You must be enjoying it!");
-	}
-}
-}
-
-/* another option?????? i don't know what's going on
-using System;
-public class Program
-{
-public void Main ()
-{
-Console.WriteLine("Is the current temperature over 30 degrees celsius?");
-string temperature;
-	temperature = Console.ReadLine( );
-	switch (temperature)
+	public void Main()
 	{
-		case yes:
-		Console.WriteLine("Heat stroke is serious. Make sure to drink water and stay inside.");
-		break;
-		case no:
-		Console.WriteLine("The weather must be nice! You must be enjoying it!");
-		break;
+		Console.WriteLine("What is the current temperature in celsius?");
+		string userTemp = Console.ReadLine();
+		int temperature = Int32.Parse(userTemp);
+		CheckTemperature(temperature);
+	}
+	public void CheckTemperature(int temperature) {
+		if(temperature >= 30) {
+			Console.WriteLine("Heat stroke is serious. Make sure to drink water and stay inside.");
+		} else if (temperature < 30) {
+			Console.WriteLine("The weather must be nice! You must be enjoying it!");
+	}
+
+	}
 }
-}
-}
-*/
