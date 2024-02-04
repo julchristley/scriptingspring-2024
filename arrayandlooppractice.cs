57b65a43cbab2ac5300c3481713a86066fa3f20e
julchristley <156378804+julchristley@users.noreply.github.com>
2024-02-04 14:00:20 -0700

    Update arrayandlooppractice.cs

--- arrayandlooppractice.cs
+++ arrayandlooppractice.cs
@@ -37,4 +37,30 @@ public class Program
 		}
 	}
 }
+
+using System;
+
+public class Program
+{
+	public void Main()
+	{
+		weapon [] weaponsObjs = {new weapon(), new weapon(), new weapon ()} ; // array because []
+		weaponsObjs[1].weaponName = "Sword";
+		weaponsObjs[2].weaponName = "Hammer";
+		
+		for(var i=0; i < weaponsObjs.Length; i++) {
+			weaponsObjs[i].powerLevel = 2;
+		
+		}
+		
+		foreach(var item in weaponsObjs) {
+			Console.WriteLine(item.weaponName);
+			Console.WriteLine(item.powerLevel);
+		}
+	}
+}
+public class weapon {
+	public string weaponName = "Axe";
+	public int powerLevel = 1;
+}
 */
