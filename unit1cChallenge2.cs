0e8be62af6e83f8a8ebaa74a7f2353a664e22530
julchristley <156378804+julchristley@users.noreply.github.com>
2024-02-05 11:12:37 -0700

    Update unit1cChallenge2.cs

--- unit1cChallenge2.cs
+++ unit1cChallenge2.cs
@@ -3,15 +3,15 @@ public class Program
 {
 	public void Main()
 	{
-		Console.WriteLine("What are your favorite foods?");
-		string favFood1 = Console.ReadLine();
-		string favFood2 = Console.ReadLine();
-		string favFood3 = Console.ReadLine();
+		Console.WriteLine("What are your favorite foods?"); // displays question
+		string favFood1 = Console.ReadLine(); // user inputs first favorite food, logs it as favFood1
+		string favFood2 = Console.ReadLine(); // user inputs 2nd favorite food, logs it as favFood2
+		string favFood3 = Console.ReadLine(); // user inputs 3rd favorite food, logs it as favFood3
 		
-		string [] userFood = {favFood1, favFood2, favFood3};
+		string [] userFood = {favFood1, favFood2, favFood3}; // strings the 3 favFoods
 		
-		foreach(var food in userFood) {
-			Console.WriteLine("I love " + food + "!");
+		foreach(var food in userFood) { // creates foreach loop, used to loop through arrays
+			Console.WriteLine("I love " + food + "!"); // writes I love (favFood)! 3 times with each different food
 		}
 	}
 }
