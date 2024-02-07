3318a91f9af329260f8f4415adea517c8d6f038a
julchristley <156378804+julchristley@users.noreply.github.com>
2024-02-07 10:36:50 -0700

    Update unit1cChallenge1.cs

--- unit1cChallenge1.cs
+++ unit1cChallenge1.cs
@@ -1,7 +1,9 @@
 /* generate random number betweeen 1 and 10, prompt user to guess the number, use a loop to give the user multiple chances to guess, provide feedback after each guess,
 indicating if guess is too high or low, when user guesses the correct number, display a congratulatory message and the number of attempts made */
 
-// displays "great job!" line and "try again" line alot, doesn't loop, no idea about the number of attempts thing
+
+
+// Problems that I am having: It seems like the generated number changes every time the user adds a new input, and the user attempt number is wrong.
 
 using System;
 public class Program
@@ -10,16 +12,18 @@ public int guessNumber;
 public int attempt = 0;
 public void Main ()
 {
-	Random random = new Random();
-	guessNumber = random.Next(1,11);
+	generateNumber();
 	questionAsk();
 }
+	public void generateNumber() {
+	Random random = new Random();
+	guessNumber = random.Next(1,11);
+	}
 	
 	public void guessingCorrect (int guessNumber, int userGuess) {
 	for (int i = userGuess; i <= 10 ; i++) {
-	if (i == guessNumber) {
+		if (i == guessNumber) {
 		Console.WriteLine("Great job! You guessed the number!");
-		questionAsk();
 		break;
 		
 		} else if (i > guessNumber) {
@@ -39,9 +43,7 @@ public void questionAsk() {
 	Console.WriteLine("Please guess a number between 1 and 10:");
 	string userInput = Console.ReadLine();
 	int userGuess = Int32.Parse(userInput);
-	attempt ++;
-	Console.WriteLine("attemtps made:" + attempt);
 	guessingCorrect (guessNumber, userGuess);
-	
+	Console.WriteLine("Attempts made:" + attempt);
 }
 }
