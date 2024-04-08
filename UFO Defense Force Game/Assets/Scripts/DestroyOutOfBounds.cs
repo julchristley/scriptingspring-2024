b4dee875dff14c6d95f792761331561867b6e59d
Juliette <julchristley@gmail.com>
2024-04-08 15:37:49 -0600

    UFO defense force lab 6a complete

---
+++ UFO Defense Force Game/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBounds = 30.0f;
+    public float lowBounds = -10.0f;
+    // Start is called before the first frame update
+    
+    void Awake()
+    {
+       // restarts update method
+       // Time.timeScale = 1;
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBounds)
+        {
+            Destroy(gameObject);
+        }  
+        else if (transform.position.z < lowBounds)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+            // stops the player from doing anything
+            // Time.timeScale = 0;
+        }
+    }
+}
---
+++ UFO Defense Force Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 16.0f;
+    public float xRange = 20.0f;
+    public Transform blaster;
+    public GameObject lazer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // sets horizontalInput to recieve values from player
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // moves player left and right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        // player constraints
+        // left side
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        // right side
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // creates lazerbolt when space bar is pressed
+            Instantiate(lazer, blaster.transform.position, lazer.transform.rotation);
+        }
+    }
+    // delete any object that hits the player 
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
