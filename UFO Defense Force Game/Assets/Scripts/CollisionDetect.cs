e614dfab7cdc093f0c8d8e1e35afd30e10b88010
Juliette Christley <julchristley@gmail.com>
2024-04-19 19:24:28 -0600

    UFO game problems with scoring

--- UFO Defense Force Game/Assets/Scripts/CollisionDetect.cs
+++ UFO Defense Force Game/Assets/Scripts/CollisionDetect.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class CollisionDetect : MonoBehaviour
 {
-   void OnTriggerEnter(Collider other)
-   {
-	   // destroys game object and the other game object it hits
+    public ScoreManager scoreManager;
+
+    public int scoreToGive;
+
+    void Start()
+    {
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+	    // destroys game object and the other game object it hits
         if (!other.gameObject.CompareTag("Pickup"))
         {
             Destroy(gameObject);
             Destroy(other.gameObject);
         }
-   }
+
+        if (other.gameObject.CompareTag("Lazer"))
+        {
+            Debug.Log("scoring??");
+            // increase score
+            scoreManager.IncreaseScore(scoreToGive);
+        }
+    }
 }
---
+++ UFO Defense Force Game/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score = 0;
+    public TextMeshProUGUI scoreText;
+
+    public void IncreaseScore(int amount)
+    {
+        score += amount;
+        UpdateScoreText();
+    }
+
+    public void DecreaseScore(int amount)
+    {
+        score -= amount;
+        UpdateScoreText();
+    }
+
+    public void UpdateScoreText()
+    {
+        Debug.Log("Score: "+ score);
+        scoreText.text = "Score: " + score;
+    }
+}
